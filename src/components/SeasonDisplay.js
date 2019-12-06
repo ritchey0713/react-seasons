32f934478d9e455f5435b7bc4055accfc81fd463
Dj <ritchey0713@gmail.com>
2019-12-06 11:47:25 -0500

    add conditional renders

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from "react"
 
 const SeasonDisplay = () => {
-  const [geoLocation, setGeoLocation] = useState({})
+  const [geoLocation, setGeoLocation] = useState({
+    latitude: "",
+    longitude: "",
+    errorMsg: ""
+  })
 
   useEffect(() => {
-    fetch(window.navigator.geolocation.getCurrentPosition((position) => {
-      setGeoLocation({latitude: position.coords.latitude, longitude: position.coords.longitude})
-    }))
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      console.log("sd")
+      setGeoLocation({...geoLocation, latitude: position.coords.latitude, longitude: position.coords.longitude})
+    }, 
+    (error) => {
+      setGeoLocation({errorMsg: error.message})
+    })
 
-  }, []) 
-  
-  return (
-    <div>
-      {geoLocation.latitude ? <p>{geoLocation.latitude}, {geoLocation.longitude}</p>: "no"}     
-    </div>
-  )
+  }, [])
+    if(geoLocation.errorMsg && !geoLocation.latitude) {
+      return (
+        <div>
+          {geoLocation.errorMsg}
+        </div>
+      )
+    } else if(!geoLocation.errorMsg && geoLocation.latitude) {
+      return (
+        <div>
+          location: {geoLocation.latitude} {geoLocation.longitude}
+        </div>
+      )
+    } else {
+      return (
+        <p>LOADING</p>
+      )
+    }
 }
 
 export default SeasonDisplay
