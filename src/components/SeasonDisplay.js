b46ac4031db860077347bd4dd95e08c6e45e6af1
Dj <ritchey0713@gmail.com>
2019-12-09 13:48:47 -0500

    add props

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import SeasonLogger from "./SeasonLogger"
 
 const SeasonDisplay = () => {
   const [geoLocation, setGeoLocation] = useState({
@@ -25,7 +26,7 @@ const SeasonDisplay = () => {
     } else if(!geoLocation.errorMsg && geoLocation.latitude) {
       return (
         <div>
-          location: {geoLocation.latitude} {geoLocation.longitude}
+          <SeasonLogger location={geoLocation}/>
         </div>
       )
     } else {
