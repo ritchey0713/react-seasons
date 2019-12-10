6c11e8cc4c4acb35c710c5aa5c777fa1e8bc11a2
Dj <ritchey0713@gmail.com>
2019-12-10 13:47:46 -0500

    create loader comp

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+  <div className="loader">
+    <figure className="figure">
+      <img src="/images/loader.gif" className="figure-img img-fluid rounded" alt="SPINNER" />
+      <figcaption className="figure-caption">LOADING</figcaption>
+    </figure>
+  </div>
+  )
+}
+
+export default Loader
